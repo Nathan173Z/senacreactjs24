99da211e63cc1d7400aadac8274e07b59c11f236
NATANAEL PEREIRA DOS SANTOS <natanael.psantos1@senacsp.edu.br>
2022-03-24 22:08:11 -0300

    Criar funcoes no component e executá-lás interpolando com{}

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,27 @@ import './App.css';
 import Header from './Header';
 
 function App() {
+
+  function handleButtonClick(){
+    alert('Click no botão');
+  }
+
+  function sum(a,b){
+    return a+b
+  }
+
   return (
    
-     <div>
+     <>
        <Header title="Meu Primeiro Component React"/>
        <Header title="Um Component Com Props"/>
         <h2>
           Amanha é Sexta ALELUIA
+          <div>Retorno da Soma:{sum(10,5)}</div>
         </h2>
+        <button onClick={handleButtonClick}>Click Me!</button>
       
-      </div>  
+      </>  
       
     
   );
