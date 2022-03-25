7f849226882f3ff8dedd32da0a720342fe97c9f1
NATANAEL PEREIRA DOS SANTOS <natanael.psantos1@senacsp.edu.br>
2022-03-24 21:30:14 -0300

    Criamos o component header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
    
      <div>
-
+       <Header />
+       <Header />
+       <Header />
         <h2>
           Amanha é Sexta ALELUIA
         </h2>
